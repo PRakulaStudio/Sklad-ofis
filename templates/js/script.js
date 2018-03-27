07d71d299a967b0f3b8b4e0adbe2cb12de2b9066
Владислав Пономарев <Public@PonomareVlad.ru>
2018-03-27 15:27:54 +0500

    Production templates

---
+++ templates/js/script.js
@@ -0,0 +1,28 @@
+var swiper = new Swiper('.slider', {
+    loop: true,
+    // autoHeight: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
+
+var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      touchRatio: 0.2,
+      slideToClickedSlide: true,
+    });
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
