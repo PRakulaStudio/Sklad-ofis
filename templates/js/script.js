ab607c6cec8dc5f3e5ef6d0c21e0d48096bd29ea
Aleksander Larionov <aleksanderlar@gmail.com>
2018-03-30 09:50:39 +0500

    Маска на телефон

--- templates/js/script.js
+++ templates/js/script.js
@@ -15,14 +15,18 @@ var swiper = new Swiper('.slider', {
 });
 
 var galleryTop = new Swiper('.gallery-top', {
-      spaceBetween: 10,
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-      spaceBetween: 10,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      touchRatio: 0.2,
-      slideToClickedSlide: true,
-    });
-    galleryTop.controller.control = galleryThumbs;
-    galleryThumbs.controller.control = galleryTop;
+    spaceBetween: 10,
+});
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+    spaceBetween: 10,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    touchRatio: 0.2,
+    slideToClickedSlide: true,
+});
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
+
+document.querySelectorAll('.message-phone').forEach(phone => {
+    phone.maskItWith('+7 (NNN) NNN-NN-NN');
+});
